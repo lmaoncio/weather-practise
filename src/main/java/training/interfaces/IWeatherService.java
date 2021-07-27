1b26ce0f79d66760239d00eca8b6aec2162c18d4
griffith <griffith@DESKTOP-L7HRQID>
2021-07-27 13:25:21 +0200

    Interfaces and methods creation

---
+++ src/main/java/training/interfaces/IWeatherService.java
@@ -0,0 +1,17 @@
+package training.interfaces;
+
+import java.util.Date;
+
+import org.json.JSONArray;
+
+public interface IWeatherService {
+	
+	String getCityWeatherByName(String city, Date dateTime);
+	
+	String getCityIDByName(String cityName);
+	
+	JSONArray getCityDataByCityID(String cityID);
+	
+	String extractCityStatusFromCityData(JSONArray cityData, Date dateTime);
+	
+}
